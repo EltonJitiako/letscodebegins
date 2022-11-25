eb0f26bda9ffdad4fecb9439af72263c21a6323c
Elton Jitiako <eltonjitiako@gmail.com>
2022-11-25 13:32:29 +0100

    change 12

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div class="main">
+        <h1>Personal site</h1>
+        <h2>Under construction!</h2>
+    </div>
+    </div>
+  );
+}
+
+export default App;
