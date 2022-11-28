a9f7f5ae1ebdc3b98f4c265491aa3f20b86c78a4
Elton Jitiako <eltonjitiako@gmail.com>
2022-11-28 20:51:35 +0100

    icons

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
+import React, { Component } from 'react';
+import { FaCodepen, FaFacebook, FaGithub, FaFreeCodeCamp, FaInstagram, FaLinkedin, FaWhatsapp, FaEnvelope, FaPhoneSquare, FaTelegram, FaLinux, FaUniversity } from "react-icons/fa";
 import './App.css';
 import waves from './movies/waves.mp4';
 
-function App() {
+
+class App extends Component {
+  render() {
   return (
     <div>
       <div className="overlay"></div>
         <video src={waves} autoPlay loop muted />
         <div className="content">
             <h1>ELTON L. JITIAKO</h1>
-            <p>contact</p>
+            <div id="icons">
+            <FaGithub />
+            <FaCodepen /> 
+            <FaFreeCodeCamp /> 
+            <FaUniversity /> 
+            <FaLinux />
+            <FaFacebook /> 
+            <FaInstagram /> 
+            <FaLinkedin /> 
+            <FaWhatsapp /> 
+            <FaTelegram /> 
+            <FaEnvelope /> 
+            <FaPhoneSquare /> 
+            </div>
+
         </div>
     </div>
   );
-}
+}}
 
 export default App;
