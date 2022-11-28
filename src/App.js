48ad8ca48e117885710622ff104cba59326d8c09
Elton Jitiako <eltonjitiako@gmail.com>
2022-11-28 16:06:53 +0100

    Bakcground Video

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import waves from './movies/waves.mp4';
 
 function App() {
   return (
     <div>
-      <div class="main">
-        <h1>Personal site</h1>
-        <h2>Under construction!</h2>
-    </div>
+      <div className="overlay"></div>
+        <video src={waves} autoPlay loop muted />
+        <div className="content">
+            <h1>ELTON L. JITIAKO</h1>
+            <p>contact</p>
+        </div>
     </div>
   );
 }
