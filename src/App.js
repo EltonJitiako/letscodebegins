6a7cfab0ada434dbfc896ebf77dcc024a2aede74
Elton Jitiako <eltonjitiako@gmail.com>
2022-11-29 13:17:18 +0100

    change responsive

--- src/App.js
+++ src/App.js
@@ -13,20 +13,45 @@ class App extends Component {
         <div className="content">
             <h1>ELTON L. JITIAKO</h1>
             <div id="icons">
-            <FaGithub />
-            <FaCodepen /> 
-            <FaFreeCodeCamp /> 
-            <FaUniversity /> 
-            <FaLinux />
-            <FaFacebook /> 
-            <FaInstagram /> 
-            <FaLinkedin /> 
-            <FaWhatsapp /> 
-            <FaTelegram /> 
-            <FaEnvelope /> 
-            <FaPhoneSquare /> 
+              <div class="col-md-4 col-sm-6 col-xs-12">
+                <FaGithub />
+              </div>
+              <div class="col-md-4 col-sm-6 col-xs-12">
+                <FaCodepen /> 
+              </div>
+              <div class="col-md-4 col-sm-6 col-xs-12">
+                <FaFreeCodeCamp /> 
+              </div>
+              <div class="col-md-4 col-sm-6 col-xs-12">
+                <FaUniversity /> 
+              </div>
+              <div class="col-md-4 col-sm-6 col-xs-12">
+                <FaLinux />
+              </div>
+              <div class="col-md-4 col-sm-6 col-xs-12">
+                <FaFacebook /> 
+              </div>
+              <div class="col-md-4 col-sm-6 col-xs-12">
+                <FaInstagram /> 
+              </div>
+              <div class="col-md-4 col-sm-6 col-xs-12">
+                <FaLinkedin />
+              </div>
+              <div class="col-md-4 col-sm-6 col-xs-12">
+                <FaWhatsapp /> 
+              </div>
+              <div class="col-md-4 col-sm-6 col-xs-12">
+                <FaTelegram /> 
+              </div>
+              <div class="col-md-4 col-sm-6 col-xs-12">
+              </div>
+              <div class="col-md-4 col-sm-6 col-xs-12">
+                <FaEnvelope /> 
+              </div>
+              <div class="col-md-4 col-sm-6 col-xs-12">
+                <FaPhoneSquare /> 
+              </div>
             </div>
-
         </div>
     </div>
   );
