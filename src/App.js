9b5ea2050519bc2a84cc168245d5cb593c276750
Elton Jitiako <eltonjitiako@gmail.com>
2022-12-01 11:44:55 +0100

    puting the links

--- src/App.js
+++ src/App.js
@@ -16,40 +16,41 @@ class App extends Component {
             <div className="row">
               <h1 className='me'>ELTON JITIAKO</h1>
               <div className={"col-lg-1 col-md-2 col-sm-3 col-xs-4 col-xxs-4 border1"}>
-                <FaGithub />
+                <a id='github' href={'https://github.com/EltonJitiako'} target='_blank' rel='noreferrer'><FaGithub /></a>
               </div>
               <div className={"col-lg-1 col-md-2 col-sm-3 col-xs-4 col-xxs-4 border1"}>
-                <FaCodepen /> 
+                <a id='codepen' href={'https://codepen.io/EltonJitiako'} target='_blank' rel='noreferrer'><FaCodepen /></a>
               </div>
               <div className={"col-lg-1 col-md-2 col-sm-3 col-xs-4 col-xxs-4 border1"}>
-                <FaFreeCodeCamp /> 
+                <a id='freeCodeCamp' href={'https://www.freecodecamp.org/fcc966dd265-203f-4783-b503-3b0774ad1e3a'} target='_blank' rel='noreferrer'><FaFreeCodeCamp /></a>
               </div>
               <div className={"col-lg-1 col-md-2 col-sm-3 col-xs-4 col-xxs-4 border1"}>
-                <FaUniversity /> 
+                <a id='JHU' href={'notyet'} target='_blank' rel='noreferrer'><FaUniversity /></a>
               </div>
               <div className={"col-lg-1 col-md-2 col-sm-3 col-xs-4 col-xxs-4 border1"}>
-                <FaLinux />
+                <a id='LPI' href={'https://cs.lpi.org/caf/Xamman/certification/verify/LPI000532255/jkm8wplmlt'} target='_blank' rel='noreferrer'><FaLinux /></a>
               </div>
               <div className={"col-lg-1 col-md-2 col-sm-3 col-xs-4 col-xxs-4 border1"}>
-                <FaFacebook /> 
+                <a id='facebook' href={'https://www.facebook.com/elton.jitiako/'} target='_blank' rel='noreferrer'><FaFacebook /></a>
               </div>
               <div className={"col-lg-1 col-md-2 col-sm-3 col-xs-4 col-xxs-4 border1"}>
-                <FaInstagram /> 
+                <a id='insta' href={'https://www.instagram.com/eltonjitiako/'} target='_blank' rel='noreferrer'><FaInstagram /></a>
               </div>
               <div className={"col-lg-1 col-md-2 col-sm-3 col-xs-4 col-xxs-4 border1"}>
-                <FaLinkedin />
+                <a id='linkedin' href={'https://www.linkedin.com/in/elton-luiz-jitiako-a175b723b/'} target='_blank' rel='noreferrer'><FaLinkedin /></a>
               </div>
               <div className={"col-lg-1 col-md-2 col-sm-3 col-xs-4 col-xxs-4 border1"}>
-                <FaWhatsapp /> 
+                <a id='whatsapp' href={'https://wa.me/34611066740'} target='_blank' rel='noreferrer'><FaWhatsapp /></a>
               </div>
               <div className={"col-lg-1 col-md-2 col-sm-3 col-xs-4 col-xxs-4 border1"}>
-                <FaTelegram /> 
+                <a id='telegram' href={'https://t.me/eltonjitiako'} target='_blank' rel='noreferrer'><FaTelegram /></a>
               </div>
               <div className={"col-lg-1 col-md-2 col-sm-3 col-xs-4 col-xxs-4 border1"}>
-                <FaEnvelope /> 
+                <a id='email' href='mailto:eltonjitiako@gmail.com?subject="link HTML'><FaEnvelope /></a>
               </div>
               <div className={"col-lg-1 col-md-2 col-sm-3 col-xs-4 col-xxs-4 border1"}>
-                <FaPhoneSquare /> 
+                <a id='phone' href="tel:+34611066740"><FaPhoneSquare /></a>
+                
               </div>
               </div>
         </div>
