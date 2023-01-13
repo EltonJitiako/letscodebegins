7260088d7c1f4954796d0b20226085042324d913
EltonJitiako <eltonjitiako@gmail.com>
2023-01-13 12:16:45 +0100

    v2 commit 6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /*********** Version 2.0 ***********/
 import React, { Component } from 'react';
 //Router
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 //Pages
 import Frontend from './Components/frontend';
 import Backend from './Components/backend';
@@ -15,8 +15,8 @@ class App extends Component {
         <div>
           <Switch>
             <Route path="/letscodebegins" exact><Layout /></Route>
-            <Route path="/letscodebegins/backend"><Backend /></Route>
-            <Route path="/letscodebegins/frontend"><Frontend /></Route>
+            <Route path="/letscodebegins/backend" exact><Backend /></Route>
+            <Route path="/letscodebegins/frontend" exact><Frontend /></Route>
           </Switch>
         </div>
       </BrowserRouter>
